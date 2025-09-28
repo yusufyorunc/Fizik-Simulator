f9a6f2395eaa34d5145be1ebf95cbb94bd8644d1
yusufyorunc <yusufyorunc65@gmail.com>
2025-09-28 18:12:22 +0300

    Update: Enhance greeting message in native-lib.cpp for Physics Simulator

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_yusufyorunc_fizik_simulator_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello! Welcome to the Physics Simulator application";
     return env->NewStringUTF(hello.c_str());
 }
